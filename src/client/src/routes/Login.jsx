6bdec592c33f0183daf3a340e601738fed1e6f3f
Madhu <shuklamadhu72@gmail.com>
2019-12-30 14:27:03 +0530

    Login page

--- src/client/src/routes/Login.jsx
+++ src/client/src/routes/Login.jsx
@@ -1,13 +1,73 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import { Form } from "react-bootstrap";
+import { Button } from "@material-ui/core/";
+import TextField from "@material-ui/core/TextField";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
 
-const Login = () => {
-  return (
-    <>
-      <div>LOGIN PAGE</div>
-      <Link to="/dash">GOTO DASHBOARD</Link>
-    </>
-  );
-};
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-export default Login;
+  handleChange(e) {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="row justify-content-center">
+        <Card className="m-5 p-5">
+          <CardHeader
+            style={{ backgroundColor: "#313F9F" }}
+            className="p-2 pl-5 pr-5 text-white rounded-pill text-center"
+            title="Login"
+          />
+          <br />
+          <CardContent>
+            <Form>
+              <TextField
+                name="email"
+                onChange={e => this.handleChange(e)}
+                type="ematextil"
+                label="Enter Email"
+              />
+              <br />
+              <TextField
+                className="mt-3"
+                name="password"
+                onChange={e => this.handleChange(e)}
+                type="email"
+                label="Enter Password"
+              />
+              <br />
+
+              <Button
+                className="mt-5"
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
+                Login
+              </Button>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+}
