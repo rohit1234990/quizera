c612537835d4ad81012679cd4c3a3fa883715444
Madhu <shuklamadhu72@gmail.com>
2019-12-31 19:59:12 +0530

    Added CreateTest Page

--- src/client/src/routes/CreateTest.jsx
+++ src/client/src/routes/CreateTest.jsx
@@ -10,6 +10,7 @@ import FormLabel from "@material-ui/core/FormLabel";
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import CardHeader from "@material-ui/core/CardHeader";
+import Grid from "@material-ui/core/Grid";
 
 export default class CreateTest extends React.Component {
   constructor(props) {
@@ -39,26 +40,26 @@ export default class CreateTest extends React.Component {
     const hrs = [];
     const mins = [];
 
-    for (let i = 0; i < 24; i + 1) {
+    for (let i = 0; i < 24; i += 1) {
       if (i.toString().length === 1) {
         const x = `0${i}`;
         hrs.push(x);
       } else hrs.push(i);
     }
 
-    for (let i = 0; i < 60; i + 1) {
+    for (let i = 0; i < 60; i += 1) {
       if (i.toString().length === 1) {
         const x = `0${i}`;
         mins.push(x);
       } else mins.push(i);
     }
     return (
-      <div style={{ marginLeft: "180px", marginRight: "200px" }}>
+      <div style={{ marginLeft: "100px", marginRight: "200px" }}>
         <div className="m-5 justify-content-center">
           <Card className="m-5 p-5">
             <CardHeader
-              style={{ backgroundColor: "#313F9F" }}
-              className="p-2 pl-5 pr-5 mb-5 text-white rounded-pill text-center"
+              style={{ backgroundColor: "#313F9F", padding: "10px" }}
+              className=" pl-5 pr-5 mb-5 text-white rounded-pill text-center"
               title="Create Test"
             />
             <TextField
@@ -75,8 +76,8 @@ export default class CreateTest extends React.Component {
               variant="outlined"
               onChange={e => this.handleChange(e)}
             />
-            <div className="row mb-5">
-              <div className="col">
+            <Grid container spacing={1} style={{ marginBottom: 25 }}>
+              <Grid item xs={6}>
                 <FormControl fullWidth>
                   <InputLabel htmlFor="age-native-simple">Hours</InputLabel>
                   <Select name="startHrs" onChange={e => this.handleChange(e)}>
@@ -86,8 +87,8 @@ export default class CreateTest extends React.Component {
                     ))}
                   </Select>
                 </FormControl>
-              </div>
-              <div className="col" md={4}>
+              </Grid>
+              <Grid item xs={6}>
                 <FormControl fullWidth>
                   <InputLabel htmlFor="age-native-simple">Mins</InputLabel>
                   <Select name="startMins" onChange={e => this.handleChange(e)}>
@@ -97,12 +98,12 @@ export default class CreateTest extends React.Component {
                     ))}
                   </Select>
                 </FormControl>
-              </div>
-            </div>
+              </Grid>
+            </Grid>
             <FormControl component="fieldset">
               <FormLabel component="legend">Jumbled Question:</FormLabel>
               <RadioGroup>
-                <div className="row">
+                <Grid item xs={12}>
                   <FormControlLabel
                     value="Yes"
                     name="jumbled"
@@ -119,7 +120,7 @@ export default class CreateTest extends React.Component {
                     labelPlacement="start"
                     onChange={e => this.handleChange(e)}
                   />
-                </div>
+                </Grid>
               </RadioGroup>
             </FormControl>
             <br />
@@ -133,7 +134,7 @@ export default class CreateTest extends React.Component {
                 id="age-native-simple"
                 style={{ marginBottom: 25 }}
               >
-                <option value="">Select...</option>
+                <option value="">..</option>
                 <option value={10}>Ten</option>
                 <option value={20}>Twenty</option>
                 <option value={30}>Thirty</option>
@@ -148,7 +149,7 @@ export default class CreateTest extends React.Component {
                 id="age-native-simple"
                 style={{ marginBottom: 25 }}
               >
-                <option value="">Select...</option>
+                <option value="">..</option>
                 <option value={10}>Ten</option>
                 <option value={20}>Twenty</option>
                 <option value={30}>Thirty</option>
@@ -163,17 +164,20 @@ export default class CreateTest extends React.Component {
                 id="age-native-simple"
                 style={{ marginBottom: 25 }}
               >
-                <option value="">Select...</option>
+                <option value="">..</option>
                 <option value={10}>Objective</option>
                 <option value={20}>subjective</option>
               </Select>
             </FormControl>
-            <div className="text-center">
+            <div style={{ textAlign: "center" }}>
               <Button
-                className="mt-5 text-white"
                 variant="contained"
                 type="submit"
-                style={{ backgroundColor: "#313F9F" }}
+                style={{
+                  backgroundColor: "#313F9F",
+                  color: "white",
+                  marginTop: "20px"
+                }}
               >
                 Submit
               </Button>
