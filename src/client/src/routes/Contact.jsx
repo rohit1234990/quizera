afe3c4de201324e0c40930a7ad93fee2203cfaab
Rohit Goyal <2211rohit@gmail.com>
2019-12-31 15:34:38 +0530

    Add Contact Us Page

--- src/client/src/routes/Contact.jsx
+++ src/client/src/routes/Contact.jsx
@@ -1,7 +1,161 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import { Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-const Contact = () => {
-  return <div>CONTACT PAGE</div>;
-};
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0
+    },
+    li: {
+      listStyle: "none"
+    }
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`
+  },
+  toolbar: {
+    flexWrap: "wrap"
+  },
+  toolbarTitle: {
+    flexGrow: 1
+  },
+  link: {
+    margin: theme.spacing(1, 1.5)
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6)
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "dark"
+        ? theme.palette.grey[700]
+        : theme.palette.grey[200]
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2)
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6)
+    }
+  }
+}));
 
-export default Contact;
+const tiers = [
+  {
+    title: "Call Us",
+    description: ["080- 1234 5678", "+91 9876543210"]
+  },
+  {
+    title: "Masai School",
+    description: [
+      "91Springboard",
+      "3rd Floor,",
+      "Hosur Road, Kormangala",
+      "Bangalore"
+    ],
+    buttonText: "Get started",
+    buttonVariant: "contained"
+  },
+  {
+    title: "Email Us",
+    description: ["admission@masai.com", "support@masai.com"]
+  }
+];
+
+export default function Contact() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Contact Us
+        </Typography>
+      </Container>
+      <Container
+        className="justify-content-center"
+        maxWidth="md"
+        component="main"
+      >
+        <Grid container spacing={5} alignItems="flex-end">
+          {tiers.map(tier => (
+            <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === "Enterprise" ? 12 : 6}
+              md={4}
+            >
+              <Card>
+                <CardHeader
+                  title={tier.title}
+                  subheader={tier.subheader}
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                  className={classes.cardHeader}
+                />
+                <CardContent>
+                  <div className={classes.cardPricing}>
+                    <Typography component="h2" variant="h3" color="textPrimary">
+                      {tier.price}
+                    </Typography>
+                  </div>
+                  <ul>
+                    {tier.description.map(line => (
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        key={line}
+                      >
+                        {line}
+                      </Typography>
+                    ))}
+                  </ul>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    fullWidth
+                    variant={tier.buttonVariant}
+                    color="primary"
+                  >
+                    <Link className="text-white" to="/">
+                      {tier.buttonText}
+                    </Link>
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
+  );
+}
