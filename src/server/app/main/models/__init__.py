e1bb2880886fd9374a96677240bffee10e8b805f
amit036 <amit68803@gmail.com>
2020-01-12 19:00:38 +0530

    Updated: init.py of Models

--- src/server/app/main/models/__init__.py
+++ src/server/app/main/models/__init__.py
@@ -1,2 +1,12 @@
 """ import all the models in the folder here"""
-from app.main.models.user import User
+from app.main.models.admin import Admin
+from app.main.models.teacher import Teacher
+from app.main.models.student import Student
+from app.main.models.batch import Batch
+from app.main.models.section import Section
+from app.main.models.test_type import TestType
+from app.main.models.quizset import Quizset
+from app.main.models.objective_questions import ObjectiveQuestions
+from app.main.models.objective_type import ObjectiveType
+from app.main.models.submission_objective import SubmissionObjective
+from app.main.models.student_questions_map import StudentQuestionMap
