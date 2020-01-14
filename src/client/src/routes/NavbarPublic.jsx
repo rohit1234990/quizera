34b6204823996a94c57e66fd8edd6c8230303050
Rohit Kumar <rohit.mnnit2017@gmail.com>
2020-01-14 19:11:55 +0530

    navbar modified

--- src/client/src/routes/NavbarPublic.jsx
+++ src/client/src/routes/NavbarPublic.jsx
@@ -6,19 +6,19 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 
-class NavBar extends React.Component {
-  useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1
-    },
-    menuButton: {
-      marginRight: theme.spacing(2)
-    },
-    title: {
-      flexGrow: 1
-    }
-  }));
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
+class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -28,13 +28,17 @@ class NavBar extends React.Component {
     const {
       location: { pathname }
     } = this.props;
+    const classes = useStyles();
+
     if (pathname.startsWith("/dash")) return null;
 
     return (
-      <div>
+      <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6">QuizEra</Typography>
+            <Typography variant="h6" className={classes.title}>
+              QuizEra
+            </Typography>
 
             <Button variant="contained" color="primary">
               Login
